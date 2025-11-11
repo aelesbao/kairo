ff0203115a1cc4f8d7be0a5244cad79986bb176f
Augusto Elesbão <aelesbao@users.noreply.github.com>
2025-11-11 12:04:45 +0100

    feat(core): fetch apps for a given scheme handler

---
+++ kiro/src/lib.rs
@@ -0,0 +1,22 @@
+use std::sync::Arc;
+
+use cosmic_mime_apps::App;
+use mime::Mime;
+
+pub mod error;
+
+pub type KiroResult<T> = Result<T, error::KiroError>;
+
+pub fn handlers_for_scheme(scheme: &str) -> KiroResult<Vec<Arc<App>>> {
+    let mime = format!("x-scheme-handler/{}", scheme)
+        .as_str()
+        .parse::<Mime>()?;
+
+    let assocs = cosmic_mime_apps::associations::by_app();
+    let apps = assocs
+        .into_values()
+        .filter(move |app| app.mime_types.contains(&mime))
+        .collect();
+
+    Ok(apps)
+}
--- kiro/src/main.rs
+++ kiro/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use kiro::{KiroResult, handlers_for_scheme};
+
+fn main() -> KiroResult<()> {
+    for app in handlers_for_scheme("http")? {
+        println!("  {:#?}", app);
+    }
+
+    Ok(())
 }
