17e7224bec4e899788658a1ab2a540b0a3f909b8
Augusto Elesbão <aelesbao@users.noreply.github.com>
2025-11-11 16:57:58 +0100

    feat(cli): list apps for a scheme or url

--- kiro/src/main.rs
+++ kiro/src/main.rs
@@ -1,8 +1,50 @@
+use clap::{Parser, Subcommand};
 use kiro::{KiroResult, handlers_for_scheme};
 
+/// Kiro
+#[derive(Parser, Debug)]
+#[command(version, author, about, long_about = None)]
+#[command(next_line_help = true)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Lists all applications that can handle the given URL scheme.
+    List {
+        /// The URL to query.
+        #[arg(
+            short,
+            long,
+            conflicts_with("scheme"),
+            required_unless_present("scheme")
+        )]
+        url: Option<url::Url>,
+
+        /// The URL scheme to query.
+        #[arg(short, long, conflicts_with("url"), required_unless_present("url"))]
+        scheme: Option<String>,
+    },
+}
+
 fn main() -> KiroResult<()> {
-    for app in handlers_for_scheme("http")? {
-        println!("  {:#?}", app);
+    let args = Args::parse();
+
+    match args.command {
+        Commands::List { url, scheme } => {
+            let scheme = match (url, scheme) {
+                (Some(url), _) => url.scheme().to_string(),
+                (_, Some(scheme)) => scheme,
+                _ => unreachable!(),
+            };
+
+            let apps = handlers_for_scheme(&scheme)?;
+            for app in apps {
+                println!("{}", app.name);
+            }
+        }
     }
 
     Ok(())
