f92dfe5ee22bf3ef9f7e90a85a8a640ec9cf785e
Augusto Elesbão <aelesbao@users.noreply.github.com>
2025-11-13 18:45:06 +0100

    feat(cli): alow overriding the desktop entries search path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::{Parser, Subcommand};
-use clap_verbosity::{Verbosity, WarnLevel};
 use console::style;
 use dialoguer::{Select, theme::ColorfulTheme};
 use kiro::{App, Result};
@@ -13,14 +12,14 @@ struct Args {
     command: Commands,
 
     #[command(flatten)]
-    verbose: Verbosity<WarnLevel>,
+    verbose: clap_verbosity::Verbosity<clap_verbosity::WarnLevel>,
 }
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    /// Lists all applications that can handle the given URL scheme.
+    /// Lists all applications that can handle a given URL scheme.
     List {
-        /// The URL to query.
+        /// The URL to query (conflicts with --scheme).
         #[arg(
             short,
             long,
@@ -29,15 +28,23 @@ enum Commands {
         )]
         url: Option<url::Url>,
 
-        /// The URL scheme to query.
+        /// The URL scheme to query (conflicts with --url).
         #[arg(short, long, conflicts_with("url"), required_unless_present("url"))]
         scheme: Option<String>,
+
+        /// Paths to search for desktop entries.
+        #[arg(long, default_value = None)]
+        search_paths: Option<Vec<std::path::PathBuf>>,
     },
 
     /// Opens the given URL with one of its associated applications.
     Open {
         /// The URL to open.
         url: url::Url,
+
+        /// Paths to search for desktop entries.
+        #[arg(long, default_value = None)]
+        search_paths: Option<Vec<std::path::PathBuf>>,
     },
 }
 
@@ -49,14 +56,18 @@ fn main() -> Result<()> {
         .init();
 
     match args.command {
-        Commands::List { url, scheme } => {
+        Commands::List {
+            url,
+            scheme,
+            search_paths,
+        } => {
             let scheme = match (url, scheme) {
                 (Some(url), _) => url.scheme().to_string(),
                 (_, Some(scheme)) => scheme,
                 _ => unreachable!(),
             };
 
-            let apps = App::handlers_for_scheme(&scheme, None, None)?;
+            let apps = App::handlers_for_scheme(&scheme, None, search_paths)?;
             println!(
                 "{: <16} {}",
                 style("App ID").bold().green(),
@@ -67,8 +78,8 @@ fn main() -> Result<()> {
             }
         }
 
-        Commands::Open { url } => {
-            let apps = App::handlers_for_scheme(url.scheme(), None, None)?;
+        Commands::Open { url, search_paths } => {
+            let apps = App::handlers_for_scheme(url.scheme(), None, search_paths)?;
             let app_names: Vec<String> = apps
                 .iter()
                 .map(|app| format!("{:<16} {}", app.appid, app.name))
